348aba300d2346a4efba88d80ccabc88a97fba52
Emeamos <Emediongamos.engr@gmail.com>
2023-09-12 22:44:47 +0100

    adding controller to route

--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { CreateNew, GetPerson } from "../controller/controller.js";
+import { CreateNew, GetPerson, UpdatePerson } from "../controller/controller.js";
 import validateStringField from "../validation/validationMiddleware.js";
 
 const Route = express.Router();
 Route.post("/",validateStringField, CreateNew);
 Route.get("/:id", GetPerson);
+Route.post("/:id",validateStringField, UpdatePerson);
 
 export default Route;
