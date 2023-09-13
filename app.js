91592f5434852c752aba0479f734e7334c8a0707
Emeamos <Emediongamos.engr@gmail.com>
2023-09-13 17:15:41 +0100

    updating app.js

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 import dbConnect from "./config/DBconnect.js";
 import Route from "./routes/route.js";
+import cors from "cors";
 import bodyParser from "body-parser";
 
 dotenv.config();
 dbConnect();
 
 const app = express();
+
+app.use(
+    cors({
+      credentials: true,
+      origin: true,
+      allowedHeaders: ["Origin", "X-Requested-With", "Content", "Accept", "Content-Type", "Authorization"],
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"]
+    })
+  );
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
